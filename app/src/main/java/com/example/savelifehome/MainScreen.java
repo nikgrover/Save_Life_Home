9b25dc350bb20616fad7383d1925e0bd26f92b05
nikgrover <nikhil.grover20@gmail.com>
2020-06-14 17:25:56 -0400

    2nd commit

--- app/src/main/java/com/example/savelifehome/MainScreen.java
+++ app/src/main/java/com/example/savelifehome/MainScreen.java
@@ -1,8 +1,13 @@
 package com.example.savelifehome;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainScreen extends AppCompatActivity {
 
@@ -10,5 +15,13 @@ public class MainScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.nav_controller_view_tag);
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_frag);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+
+
     }
 }
+
+
--- app/src/main/java/com/example/savelifehome/Signup.java
+++ app/src/main/java/com/example/savelifehome/Signup.java
@@ -1,12 +1,20 @@
 package com.example.savelifehome;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class Signup extends AppCompatActivity {
+
+    EditText regemaol, regpassword;
+    Button signup;
+    TextView toLog;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,9 +22,8 @@ public class Signup extends AppCompatActivity {
         setContentView(R.layout.activity_signup);
     }
 
-    public void home(View view)
-    {
-        Intent home = new Intent(this,MainScreen.class);
+    public void home(View view) {
+        Intent home = new Intent(this, MainScreen.class);
         startActivity(home);
     }
 }
